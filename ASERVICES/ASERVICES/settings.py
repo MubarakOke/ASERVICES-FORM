6f06e1bf070d4b6dbf51c1f10ea644b2042f6f2f
MubarakOke <okeolamubarak18@gmail.com>
2021-08-14 20:28:28 -0700

    heroku config

--- ASERVICES/ASERVICES/settings.py
+++ ASERVICES/ASERVICES/settings.py
@@ -23,9 +23,9 @@ from .secret_settings import SECRET_KEY
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1/', ]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
